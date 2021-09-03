81e173adb42bfdda12db8a4b39f92b644416c93c
lekoal <coreglus@gmail.com>
2021-09-03 12:33:19 +0300

    Initial commit

---
+++ src/ru/geekbrains/java_core_homework_9_2/Main.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.java_core_homework_9_2;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/ru/geekbrains/java_core_homework_9_2/MyArraySizeException.java
@@ -0,0 +1,4 @@
+package ru.geekbrains.java_core_homework_9_2;
+
+public class MyArraySizeException {
+}
